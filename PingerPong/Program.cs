31457d2f4b68260f4ddf38dc17f5108527ac36af
Asketron <askewinkler@gmail.com>
2026-09-11 15:29:07 +0200

    Prøver at lave blocke i den anden ende af battet som man kan ramme og ødelægge for points

--- PingerPong/Program.cs
+++ PingerPong/Program.cs
@@ -6,9 +6,23 @@ using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Diagnostics;
 using System.Reflection.Metadata;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Globalization;
+using System.Collections.Generic;
+
+
+public class Block
+{
+    public int x1;
+    public int x2;
+    public int y1;
+    public int y2;
+}
 
 public class JepsiPepsi
 {
+
+    List<Block> blocks = new List<Block>();
     
     int xKant = 148;
     int yKant = 40;
@@ -17,7 +31,13 @@ public class JepsiPepsi
     float batPos = -1;
     int batSpeed = 1;
     float ballspeed = 0.3f;
-    
+
+    bool ballRunning = false;
+
+    int blockX = 2;
+    int blockY = 8;
+    int blockNumX = 3;
+    int blockNumY = 5;
 
     // yKant = 40, default
     // xKant = 148, default
@@ -33,6 +53,11 @@ public class JepsiPepsi
         int ballx = jepsi.xKant / 2;
         int lastY = bally;
         int lastX = ballx;
+
+       
+
+        //Console.ForegroundColor = ConsoleColor.Red
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
 
         BaneKant(jepsi.xKant, jepsi.yKant);
         PlayerBat(jepsi.xKant, jepsi.yKant, jepsi.batLængde, 0);
@@ -55,50 +80,23 @@ public class JepsiPepsi
             ballAng = rnd.Next(280, 360);
         }
 
-
-
+        GetBlocks(jepsi.blockX, jepsi.blockY, jepsi.blockNumX, jepsi.blockNumY, jepsi.xKant, ref jepsi.blocks);
+        MakeBlocks(jepsi.blocks);
         double x = 0;
         double y = 0;
 
         jepsi.igang = true;
-
-       
-        
         while (jepsi.igang)
         {
+            bool hit = false;
 
-              if (jepsi.xKant / 20 - 2 <= ballx && jepsi.xKant / 20 + 3 >= ballx && jepsi.batPos + jepsi.batLængde / 2 >= bally && jepsi.batPos - jepsi.batLængde / 2 <= bally)
-                {
-                    ballAng = (180f - ballAng + 360f) % 360f;
+            BallBounce(jepsi.xKant / 20, (int)(jepsi.batPos - jepsi.batLængde / 2) - 1, jepsi.xKant / 20 + 1, (int)(jepsi.batPos + jepsi.batLængde / 2) + 1, ref ballAng, ref ballx, ref bally, ref hit);
 
-                      y = 0;
-                     x = 0;
-                if (jepsi.xKant / 20 - 2 >= ballx)
-                {
-                    ballx = jepsi.xKant / 20 - 3;
-                }
-                else
-                {
-                    ballx = jepsi.xKant / 20 + 4;
-                }
-            }
-            else if (jepsi.xKant / 20 <= ballx && jepsi.xKant / 20 + 1 >= ballx && (jepsi.batPos + jepsi.batLængde / 2 + 1 == bally || jepsi.batPos - jepsi.batLængde / 2 - 1 == bally))
+            if (hit == true)
             {
-                ballAng = (360 - ballAng) % 360;
-
                 y = 0;
                 x = 0;
-                if (jepsi.batPos + 1 + jepsi.batLængde / 2 == bally)
-                {
-                    bally = (int)Math.Round(jepsi.batPos + jepsi.batLængde / 2 + 2);
-                }
-                else
-                {
-                    bally = (int)Math.Round(jepsi.batPos - jepsi.batLængde / 2 - 2);
-                }
-
             }
-
 
 
             if (1 >= ballx || jepsi.xKant - 3 <= ballx)//´´_
@@ -232,7 +230,7 @@ public class JepsiPepsi
        
 
         Console.SetCursorPosition(x, y);
-        Console.Write("o");
+        Console.Write("●");
         Console.CursorVisible = false;
         
         
@@ -277,7 +275,9 @@ public class JepsiPepsi
     public static void BaneKant(int xKant, int yKant)
     {
         //top linje
+        Console.ForegroundColor = ConsoleColor.Red;
         Console.Write("┌");
+        Console.ForegroundColor = ConsoleColor.White;
         for (int i = 0; i < xKant; i++)
         {
             Console.Write("─");
@@ -286,7 +286,10 @@ public class JepsiPepsi
         //Midten
         for (int i = 0; i < yKant - 2; i++)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.Write("\n│");
+            Console.ForegroundColor = ConsoleColor.White;
+
             for (int o = 0; o < xKant; o++)
             {
                 Console.Write(" ");
@@ -295,7 +298,9 @@ public class JepsiPepsi
         }
         // Bunden linje
         Console.Write("\n");
+        Console.ForegroundColor = ConsoleColor.Red;
         Console.Write("└");
+        Console.ForegroundColor = ConsoleColor.White;
         for (int i = 0; i < xKant; i++)
         {
             Console.Write("─");
@@ -305,5 +310,78 @@ public class JepsiPepsi
         
     }
 
-    
+
+    public static void BallBounce(int x1, int y1, int x2, int y2, ref float ang, ref int ballx, ref int bally, ref bool hit)
+    {
+        Random rnd = new Random();
+
+        if (x1 - 2 <= ballx && x2 + 2 >= ballx && y2 >= bally && y1 <= bally) // med top/bund af bat   _
+        {
+
+            hit = true;
+
+            int i = rnd.Next(-10, 10);
+
+            ang = (180f - ang + 360f) % 360f + i;
+
+            
+            if (x1 - 2 >= ballx) // top
+            {
+                ballx = x1 - 3; // top
+            }
+            else
+            {
+                ballx = x2 + 3; // bund
+            }
+        }
+        else if (x1 <= ballx && x2 >= ballx && (y2 == bally || y1 == bally)) // Kollidere med Siderne på bat  |
+        {
+
+            hit = true;
+            int i = rnd.Next(-10, 10);
+
+            ang = (360 - ang) % 360 + i;
+
+            
+            if (y2 == bally)
+            {
+                bally = y2 + 1; // højer
+            }
+            else
+            {
+                bally = y1 - 1; // venstre
+            }
+
+        }
+    }
+
+    public static void GetBlocks(int blockX, int blockY, int blockNumX, int blockNumY, int xKant, ref List<Block> blocks)
+    {
+        for (int i = 0; i < blockNumX; i++)
+        {
+            for (int o = 0; o < blockNumY; i++)
+            {
+                blocks.Add(new Block()
+                {
+                    x1 = xKant - blockX - blockX * i,
+                    x2 = xKant - blockX * i,
+                    y1 = o * blockY + 1,
+                    y2 = o * blockY + blockY + 1
+
+                });
+
+            }
+
+
+
+
+        }
+    }
+
+    public static void MakeBlocks(List<Block> blocks)
+    {
+
+    }
+
+
 }
